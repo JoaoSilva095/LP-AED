fe62f2fb1baf848ada185c23bb1033b86e445fd0
RafaeljesusUFP <2024103592@ufp.edu.pt>
2025-11-22 18:09:51 +0000

    Implement dynamic string matrix in main function
    Refactored main function to create and manage a dynamic string matrix, including memory allocation and deallocation.

--- main.c
+++ main.c
@@ -1,47 +1,20 @@
-//
-// Created by Rafael on 22/11/2025.
-//
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "struct.h"
 
 
-MATRIX_STR* criar_matriz(int capacidade) {
-    MATRIX_STR *matriz;// apontador da struct
-    matriz = (MATRIX_STR *)malloc(sizeof(MATRIX_STR));
-    matriz->dados = (char **)malloc(capacidade * sizeof(char *)); // cria espaço para os apontadores de char
-    matriz->linhas = 0;
-    matriz->capacidade = capacidade;
-    return matriz;
-
-
-}
-void insert_str(MATRIX_STR* matriz, const char* str) {
-    if (matriz->linhas >= matriz->capacidade) {
-        matriz->capacidade *= 2; // Multiplica o tamanho da capacidade
-        matriz->dados = (char**) realloc(matriz->dados, matriz->capacidade * sizeof(char*));
-        //Pede mais memória para matriz e atualiza o apontador
-    }
-    // Duplica e insere a string
-    matriz->dados[matriz->linhas] = strdup(str); // strdup aloca memória e copia a string
-    matriz->linhas++;
-}
-void lista_da_matriz(MATRIX_STR *matriz) {
-    if (matriz == NULL || matriz->dados == NULL) {
-        printf("Matriz já foi libertada.\n");
-        return;
-    }
-    for (int i = 0; i < matriz->linhas; i++) {
-        printf("%s\n", matriz->dados[i]);
-    }
+int main(void) {
+    MATRIX_STR* matriz = criar_matriz(2); // cria matriz com capacidade inicial de 2
+    insert_str(matriz, "ola");
+    insert_str(matriz, "mundo");
+    insert_str(matriz, "isto");
+    insert_str(matriz, "e");;
+    insert_str(matriz, "uma");;
+    insert_str(matriz, "matriz");
+    lista_da_matriz(matriz);
+    libertar_matriz(&matriz); // liberta a memória mas so funciona com endereço Porque nao sei ...
+    lista_da_matriz(matriz);
+    return 0;
 }
 
-void libertar_matriz(MATRIX_STR* matriz) {
-    for (int i = 0; i < matriz->linhas; i++) {
-        free(matriz->dados[i]); // Liberta cada string individualmente
-    }
-    free(matriz->dados); // Liberta o array de apontadores
-    free(matriz); // Liberta a struct
-
-}
